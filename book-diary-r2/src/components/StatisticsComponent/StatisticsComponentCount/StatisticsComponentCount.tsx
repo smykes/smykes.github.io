3341632fb064a18f51f5bafdf124e342cfa603a9
Jeff Smykil <jeff.smykil@gmail.com>
2025-01-13 12:38:03 -0600

    removed files that are now served from netlify

--- book-diary-r2/src/components/StatisticsComponent/StatisticsComponentCount/StatisticsComponentCount.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const StatisticsComponentCount = () => {
-  return <>"test"</>;
-};
-
-export default StatisticsComponentCount;
--- book-diary-r2/src/components/StatisticsComponent/StatisticsComponentLabel/StatisticsComponentLabel.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const StatisticsComponentLabel = () => {
-  return <div className="card-title">test</div>;
-};
-
-export default StatisticsComponentLabel;
